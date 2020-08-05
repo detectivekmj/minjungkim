91f3de266cfa69c8c57906e7dc3e5d217ade733f
김민중 <>
2020-08-05 14:20:33 +0900

    ts, babel, webpack

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,17 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `MinJungKim Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `AlexKim`,
+      summary: `web developer.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `MinJungKim's blog`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      github: `detectivekmj`,
     },
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        // isTSX: true, // defaults to false
+        // allExtensions: true, // defaults to false
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
